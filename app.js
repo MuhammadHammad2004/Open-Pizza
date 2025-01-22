898fab44cb8f3f620e4c56fdc9b83845e1b29499
Muhammad Hammad <isddhaddd@gmail.com>
2025-01-22 01:17:02 -0800

    Work completed

--- app.js
+++ app.js
@@ -3,46 +3,55 @@ const pizzaMenu = [
     name: "Margherita Classic",
     description: "Fresh tomatoes, mozzarella, basil, olive oil",
     price: "$14.99",
+    price_btn: "Add",
   },
   {
     name: "Pepperoni Supreme",
     description: "Pepperoni, extra cheese, Italian herbs",
     price: "$16.99",
+    price_btn: "Add",
   },
   {
     name: "Vegetarian Delight",
     description: "Bell peppers, mushrooms, onions, olives, tomatoes",
     price: "$15.99",
+    price_btn: "Add",
   },
   {
     name: "BBQ Chicken",
     description: "Grilled chicken, BBQ sauce, red onions, cilantro",
     price: "$17.99",
+    price_btn: "Add",
   },
   {
     name: "Margherita Classic",
     description: "Fresh tomatoes, mozzarella, basil, olive oil",
     price: "$14.99",
+    price_btn: "Add",
   },
   {
     name: "Pepperoni Supreme",
     description: "Pepperoni, extra cheese, Italian herbs",
     price: "$16.99",
+    price_btn: "Add",
   },
   {
     name: "Vegetarian Delight",
     description: "Bell peppers, mushrooms, onions, olives, tomatoes",
     price: "$15.99",
+    price_btn: "Add",
   },
   {
     name: "BBQ Chicken",
     description: "Grilled chicken, BBQ sauce, red onions, cilantro",
     price: "$17.99",
+    price_btn: "Add",
   },
   {
     name: "BBQ Chicken",
     description: "Grilled chicken, BBQ sauce, red onions, cilantro",
     price: "$17.99",
+    price_btn: "Add",
   },
 ];
 
@@ -56,7 +65,7 @@ function populateMenu() {
     menuItem.innerHTML = `
         <h3>${pizza.name}</h3>
         <p>${pizza.description}</p>
-        <p class="price">${pizza.price}</p>
+        <p class="price">${pizza.price} </p>
       `;
     menuGrid.appendChild(menuItem);
   });
